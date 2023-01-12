c725407545c351942cd8c6518188e35006ce44b3
MauriceDarner <mdar615@g.educaand.es>
2023-01-12 22:35:56 +0100

    Changed to generic types. Doubts -     When the if check in main is run with numbers instead of listOf it works.

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,24 +1,42 @@
-interface Iterator {
-    fun run(pila: Pila) = pila.stack.iterator()
-}
 
-class Pila(var stack: List<Any>): Iterator {
+class Pila<T>  {
+    private var stack: List<T> = emptyList()
+
     fun top() = stack.last()
 
-    fun push(newMember: Any) {
+    fun push(newMember: T) {
         stack = stack.plus(newMember)
     }
 
     fun pop() {
         stack = stack.dropLast(1)
+
     }
 
     fun empty() = stack.isEmpty()
 
-    fun reverse() = stack.reversed()
 
     override fun toString(): String {
         return "Top: ${top()}, Empty: ${empty()}, Contents: $stack"
     }
+}
+
+fun <T> reverse(numbers: List<T>): List<T> {
+    val normalPila = Pila<T>()
+    val reversePila = Pila<T>()
+    var reverseList = emptyList<T>()
+
+    for (item in numbers.iterator()) {
+        normalPila.push(item)
+    }
+    for (item in numbers.iterator()) {
+        reversePila.push(normalPila.top())
+        normalPila.pop()
+    }
+    for (item in numbers.iterator()) {
+        reverseList = reverseList.plus(reversePila.top())
+        reversePila.pop()
+    }
+    return reverseList
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,13 @@
 
 fun main() {
     val numbers = listOf("one", "two", "three", "four")
-    val numbersRev = numbers.reversed()
+    val numbersRev: List<String> = reverse(numbers)
 
-    if (!listOf("four", "three", "two", "one").equals(numbersRev)) {
+    if (!numbers.equals(numbersRev))
         println("Error")
-    }
-    else {
+
+    else
         println("Correcto")
-        println(numbersRev)
-    }
 
-    val myPila = Pila(emptyList())
-    println(myPila.empty())
-    myPila.push("one")
-    myPila.push("two")
-    myPila.push("three")
-    myPila.push("four")
-    myPila.push("five")
 
-    println(myPila.top())
-    myPila.pop()
-    println(myPila)
-
-    val myPilaReverse = myPila.reverse()
-
-    if (!listOf("four", "three", "two", "one").equals(myPilaReverse)) {
-        println("Error")
-    }
-    else {
-        println("Correcto")
-        println(myPilaReverse)
-    }
 }
